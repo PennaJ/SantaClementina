7c12b89118a1e02e82d0eadaa4df8d75911ce09e
PennaJ <julianpenna96@gmail.com>
2023-05-22 10:54:08 -0300

    agregado google maps API, correccion estilos, avance con el index

--- public/js/main.js
+++ public/js/main.js
@@ -15,3 +15,24 @@ window.onload = function () {
 		mobile_menu.classList.toggle('is-active');
 	});
 }
+
+	// Initialize and add the map
+	let map;
+
+	// Initialize and add the map
+function initMap() {
+	// The location of Uluru
+	const SantaClementina = { lat: -35.231594, lng: -63.5921452 };
+	// The map, centered at Uluru
+	const map = new google.maps.Map(document.getElementById("map"), {
+	  zoom: 19,
+	  center: SantaClementina,
+	});
+	// The marker, positioned at Uluru
+	// const marker = new google.maps.Marker({
+	//   position: SantaClementina,
+	//   map: map,
+	// });
+  }
+  
+  window.initMap = initMap;
